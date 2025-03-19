569a128bd6ce59ea83b1fcd668cec915d9bb9024
Sharr0wkyn <egor.knysh2018@yandex.ru>
2025-03-19 13:55:01 +0700

    avatar

--- app/models.py
+++ app/models.py
@@ -5,6 +5,8 @@ from app import db
 from datetime import datetime, timezone
 from app import login
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from hashlib import md5
     
 @login.user_loader
 def load_user(id):
@@ -12,11 +14,15 @@ def load_user(id):
 
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+    about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc))
+    # Анонимная функция
 
     posts: so.WriteOnlyMapped['Post'] = so.relationship(
         back_populates='author')
@@ -30,6 +36,10 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
     
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
